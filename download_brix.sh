4fbd98a581a4f5b48b7ca0d212d2f6f8329c8703
mgrandinetti <m.grandinetti@gmail.com>
2017-02-19 17:38:14 +0100

    Release 1.4
    Add download_brix.sh
    Add CoreDisplay Patcher

---
+++ download_brix.sh
@@ -0,0 +1,32 @@
+
+#set -x
+
+function download()
+{
+    echo "downloading $2:"
+    curl --location --silent --output /tmp/org.rehabman.download.txt https://bitbucket.org/RehabMan/$1/downloads
+    scrape=`grep -o -m 1 "/RehabMan/$1/downloads/$2.*\.zip" /tmp/org.rehabman.download.txt|perl -ne 'print $1 if /(.*)\"/'`
+    url=https://bitbucket.org$scrape
+    echo $url
+    if [ "$3" == "" ]; then
+        curl --remote-name --progress-bar --location "$url"
+    else
+        curl --output "$3" --progress-bar --location "$url"
+    fi
+    echo
+}
+
+if [ ! -d ./downloads ]; then mkdir ./downloads; fi && rm -Rf downloads/* && cd ./downloads
+
+# download kexts
+mkdir ./kexts && cd ./kexts
+download os-x-fakesmc-kozlek RehabMan-FakeSMC
+download os-x-eapd-codec-commander RehabMan-CodecCommander
+download os-x-fake-pci-id RehabMan-FakePCIID
+
+https://github.com/vit9696/AppleALC/releases/latest
+
+#download os-x-acpi-debug RehabMan-Debug
+cd ..
+
+# download tools
